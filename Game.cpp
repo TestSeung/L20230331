94aa4d18ed9613477155dfafc9b1a5f6cd9b1ff8
TestSeung <sjlim589@hanmail.net>
2023-03-31 17:27:19 +0900

    화면지우기추가

--- Game.cpp
+++ Game.cpp
@@ -15,25 +15,29 @@ int main()
 	{ 1,1,1,1,1,1,1,1,1,1 }
 	};
 
-	int PlayerX = 1;
-	int PlayerY = 1;
-
-	for (int Y = 0; Y < 10; ++Y)
+	while (true)
 	{
-		for (int X = 0; X < 10; ++X)
-			if (PlayerX == X && PlayerY == Y)
-			{
-				cout << "P";
-			}
-			else if (Map[Y][X] == 1)
-			{
-				cout << "# ";
-			}
-			else if (Map[Y][X] == 0)
-			{
-				cout << "  ";
-			}
-		cout << endl;
+		int PlayerX = 1;
+		int PlayerY = 1;
+
+		for (int Y = 0; Y < 10; ++Y)
+		{
+			for (int X = 0; X < 10; ++X)
+				if (PlayerX == X && PlayerY == Y)
+				{
+					cout << "P ";
+				}
+				else if (Map[Y][X] == 1)
+				{
+					cout << "# ";
+				}
+				else if (Map[Y][X] == 0)
+				{
+					cout << "  ";
+				}
+			cout << endl;
+		}
+		system("cls");
 	}
-	
+	return 0;
 }
